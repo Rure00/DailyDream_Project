eb1cdbdd74c96dea4efde071840318f93349d8d7
Rure00 <asnjkd@naver.com>
2023-03-02 18:20:47 +0900

    정리

---
+++ dailydream/src/main/java/com/dailydream/dailydream/DailydreamApplication.java
@@ -0,0 +1,13 @@
+package com.dailydream.dailydream;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DailydreamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DailydreamApplication.class, args);
+	}
+
+}
